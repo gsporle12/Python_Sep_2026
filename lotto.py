75b83e99a9a2c12ff152cf47913f0489b87f260c
George Sporle <gsporle12@gmail.com>
2026-09-15 16:57:30 +0100

    Updated lotto as per ex5_3

--- lotto.py
+++ lotto.py
@@ -4,13 +4,21 @@
 # Description: This script will generate 6 random lottery numbers
 import random
 
-lotto = [] # Create an empty list
+# lotto = [] # Create an empty list
+
+# while len(lotto) < 6:
+#    num = random.randint(1, 50)
+#    if num not in lotto:
+#        lotto.append(num)
+#    else:
+#        print("Duplicate number generated: ", num)
+
+# print("Lottery numbers =", lotto)
+
+lotto = set()
 
 while len(lotto) < 6:
     num = random.randint(1, 50)
-    if num not in lotto:
-        lotto.append(num)
-    else:
-        print("Duplicate number generated: ", num)
+    lotto.add(num)
 
-print("Lottery numbers =", lotto)
+print("Lottery numbers: ", sorted(lotto))
