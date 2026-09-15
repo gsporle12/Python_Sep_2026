1dc8a303d699795d343f643641861b13bf2e805e
George Sporle <gsporle12@gmail.com>
2026-09-15 10:08:04 +0100

    Added Donalds code for the demo_platform script.

--- demo_platform.py
+++ demo_platform.py
@@ -4,7 +4,23 @@
 # Description: This script will demo HOWTO find out
 # which platform your script is running on!
 
-import sys
-
+import sysimport platform
+import os
 if sys.platform == "win32":
-    print("You are running on Windows")
+        print("You are running on Windows")
+            h_dir = os.environ['HOMEPATH']
+            h_dir = os.path.join(h_dir, 'photos')
+elif sys.platform == "linux" or sys.platform == "Darwin":
+    print("You are running on Linux")
+        h_dir = os.environ['HOME']
+            h_dir = os.path.join(h_dir, 'photos')
+
+            print("Photos folder is: " + h_dir)
+
+
+            if platform.system() == "Windows":
+                    print("You are running on Windows")
+            elif platform.system() == "Darwin":
+                    print("You are running on MacOS")
+            elif platform.system() == "Linux":
+                    print("You are running on Linux")
