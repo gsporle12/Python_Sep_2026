b91921d09f67231329e505e30ba146d641aea7d6
George Sporle <gsporle12@gmail.com>
2026-09-16 11:00:08 +0100

    Updated demo_regex.py with example of match and fullmatch and flags

--- demo_regex.py
+++ demo_regex.py
@@ -30,8 +30,11 @@ for line in fh_in:
     # m = re.search(r"[aeiou]{4}", line) # Match lines with 4 consecutive VOWELS
     # m = re.search(r"^[A-Z].*[A-Z]$", line) # Match lines START/END with a CAPITAL
     # m = re.search(r"^[A-Z].{4}[A-Z]$", line) # Match lines START/END with a CAPITAL with 4 chars in between
-    m = re.search(r"^(.)(.).\2\1$", line) # Match lines 5 char palindromes
-    # m = re.search(r"([A-Z]).*\1$", line) # Match lines START/END with the SAME CAPITAL
+    # m = re.search(r"^(.)(.).\2\1$", line) # Match lines 5 char palindromes
+    # m = re.search(r"([A-Z]).*\1$", line, flags=re.I) # Match lines START/END with the SAME CAPITAL
+    # m = re.match(r"(.)(.).\2\1$", line) # match() AUTO matches START of LINE
+    m = re.fullmatch(r"^(.)(.).\2\1$\n", line) # Match ENTIRE line including HIDDEN chars
+
 
     if m:
         print(f"Matched {m.group()} on {line.rstrip()} at {m.start()}-{m.end()}, "
