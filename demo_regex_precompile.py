f016a5e88c050699d6834696cae2238286ef8bc8
George Sporle <gsporle12@gmail.com>
2026-09-17 16:14:30 +0100

    Added error handling

--- demo_regex_precompile.py
+++ demo_regex_precompile.py
@@ -4,15 +4,43 @@
 # Description: This script will demo HOWTO match text using str testing and
 # pattern matching using Regex and re.py module.
 
+import sys
 import re
-fh_in = open(r"C:\labs\words", mode="rt")
+from re import search
 
-reobj = re.compile(r"^.{19}$") # Compiles pattern ONLY ONCE
 
-for line in fh_in:
-    # m = re.search(r"^.{19}$", line)  # Match lines with exactly 19 chars
-    m = reobj.search(line)  # Match using PRECOMPILED Pattern
-    if m:
-        print(f"Matched {m.group()} on {line.rstrip()} at {m.start()}-{m.end()}")
+def search_pattern(pattern=r"^.{19}$", file=r"c:\labs\words"):
+    try:
+        fh_in = open(file, mode="rt")
+    except FileNotFoundError as err:
+        print(f"Error: {err.args[0]}, {err.args[1]}, {err.filename}", file=sys.stderr)
+        sys.exit(1)
+    except PermissionError as err:
+        print(f"Error: {err.args[0]}, {err.args[1]}, {err.filename}", file=sys.stderr)
+        sys.exit(2)
+    except Exception as err:
+        print(f"Some other error occurred - Investigate", file=sys.stderr)
+        sys.exit(3)
+    else:
+        # Executes if try block succeeds
+        reobj = re.compile(pattern) # Compiles pattern ONLY ONCE
 
-fh_in.close() # Close file handle.
+        for line in fh_in:
+            m = reobj.search(line)  # Match using PRECOMPILED Pattern
+            if m:
+                print(f"Matched {m.group()} on {line.rstrip()} at {m.start()}-{m.end()}")
+
+        fh_in.close() # Close file handle.
+    finally:
+        # Always executes!
+        print("And now for something completely different..")
+        return None
+
+def main():
+    search_pattern()
+    return None
+
+# Namespace trick
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
